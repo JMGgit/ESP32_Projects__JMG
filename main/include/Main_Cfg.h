0e2a7e191bdc5fca30472dea1fc4eebf58563e7e
Jean-Martin George <jeanmartin.george@gmail.com>
2017-02-27 19:55:33 +0100

    - Wifi SSID configuration is now done in Wifi_SSID_Cfg.h - UART config parameter deleted
    Signed-off-by: Jean-Martin George <jeanmartin.george@gmail.com>

--- main/include/Main_Cfg.h
+++ main/include/Main_Cfg.h
@@ -8,27 +8,14 @@
 #ifndef MAIN_CFG_H_
 #define MAIN_CFG_H_
 
+#include "Wifi_SSID_Cfg.h"
+
 
 /********** CTL LED *********/
 #define UC_CTRL_LED_GPIO				GPIO_NUM_5
 #define TEST_LED_ARTNET					GPIO_NUM_4
 #define TEST_LED_LEDCTRL				GPIO_NUM_25
 
-/********* WIFI *********/
-#define WIFI_SSID 						"Leo-Wohnung"
-#define WIFI_PASSWORD 					"ObereBurghalde33"
-
-/********* UART *********/
-#define UART1_TX_GPIO					GPIO_NUM_17
-#define UART1_RX_GPIO					GPIO_NUM_16
-#define UART1_RTS_GPIO					UART_PIN_NO_CHANGE
-#define UART1_CTS_GPIO					UART_PIN_NO_CHANGE
-/* Rx buffer not necessary but has to be greater UART_FIFO_LEN */
-#define UART1_RX_BUFFER_LENGTH			(UART_FIFO_LEN + 1)
-/* Tx buffer not needed for now */
-#define UART1_TX_BUFFER_LENGTH			0
-/* According to "Glediator protocol" */
-#define UART_LED_FIRST_BYTE				1
 
 /********* LEDS *********/
 #define LED_TABLE_SIZE_X				42
---
+++ main/include/Wifi_SSID_Cfg.h
@@ -0,0 +1,16 @@
+/*
+ * Wifi_SSID_Cfg.h
+ *
+ *  Created on: 27.02.2017
+ *      Author: Jean-Martin George
+ */
+
+#ifndef WIFI_SSID_CFG_H_
+#define WIFI_SSID_CFG_H_
+
+
+/********* WIFI *********/
+#define WIFI_SSID 						"myWifiSSID"
+#define WIFI_PASSWORD 					"myPassword"
+
+#endif /* WIFI_SSID_CFG_H_ */
