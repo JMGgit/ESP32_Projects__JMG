c9d2b9608fa51db6511cc15652a5074255a2c431
Jean-Martin George <jeanmartin.george@gmail.com>
2017-02-27 21:54:24 +0100

    SPI cleanup
    Signed-off-by: Jean-Martin George <jeanmartin.george@gmail.com>

--- components/Drivers/APA102.c
+++ components/Drivers/APA102.c
@@ -11,7 +11,15 @@
 #include "driver/gpio.h"
 
 #if (LED_TYPE == LED_TYPE_APA102)
-//#include "LEDMatrix.h"
+
+/* SPI parameters */
+spi_bus_config_t spiBusConfig;
+spi_device_interface_config_t spiDeviceInterfaceConfig;
+spi_transaction_t spiTransaction;
+spi_device_handle_t spiDeviceHandle;
+#define SPI_TX_BUFFER_LENGTH (START_FRAME_LENGTH + (4 * NUMBER_OF_LEDS)+ STOP_FRAME_LENGTH)
+uint8_t spiTxBuffer[SPI_TX_BUFFER_LENGTH];
+
 
 #if (RGB_LED_ORDER == RGB_LED_ORDER__CONFIGURABLE)
 static uint8_t RGBLedOrder;
@@ -20,9 +28,7 @@ static uint8_t RGBLedOrder_EEPROM EEMEM;
 static uint8_t updateEnabled = TRUE;
 static uint8_t globalBrightness;
 
-RGB_Color_t GS_Data[NUMBER_OF_LEDS];
-/* - cannot be declared as static: ATMega limitation??
- * - defined as struct to save run time compared to simple buffer */
+RGB_Color_t GS_Data[NUMBER_OF_LEDS]; /* defined as struct to save run time compared to simple buffer */
 
 uint8_t APA102__getRGBLedOrder (void)
 {
@@ -53,20 +59,12 @@ void APA102__toggleRGBLedOrder (void)
 #endif
 
 
-spi_transaction_t spiTransaction;
-spi_device_handle_t spi;
-
-#define TX_BUFFER_LENGTH	(START_FRAME_LENGTH + (4 * NUMBER_OF_LEDS)+ STOP_FRAME_LENGTH)
-
-uint8_t txBuffer[TX_BUFFER_LENGTH];
 
 void APA102__updateAll (void)
 {
-    esp_err_t ret;
-	memset(txBuffer, 0, sizeof(txBuffer));
 	memset(&spiTransaction, 0, sizeof(spiTransaction));
-	spiTransaction.tx_buffer = &txBuffer[0];
-	spiTransaction.length = TX_BUFFER_LENGTH * 8;
+	spiTransaction.tx_buffer = &spiTxBuffer[0];
+	spiTransaction.length = SPI_TX_BUFFER_LENGTH * 8;
 
 	uint16_t it, bufferIdx;
 
@@ -75,47 +73,45 @@ void APA102__updateAll (void)
 	/* START FRAME*/
 	for (it = 0; it < START_FRAME_LENGTH; it++)
 	{
-		txBuffer[bufferIdx++] = 0;
+		spiTxBuffer[bufferIdx++] = 0;
 	}
-
-	//bufferIdx = bufferIdx + START_FRAME_LENGTH;
 
 	/* LED FRAMES */
 	for (it = 0; it < NUMBER_OF_LEDS; it++)
 	{
-		txBuffer[bufferIdx++] = 0xE0 | globalBrightness;
+		spiTxBuffer[bufferIdx++] = 0xE0 | globalBrightness;
 
 		if (APA102__getRGBLedOrder() == RGB_LED_ORDER__BLUE_GREEN_RED)
 		{
-			txBuffer[bufferIdx++] = GS_Data[it].blue;
+			spiTxBuffer[bufferIdx++] = GS_Data[it].blue;
 		}
 		else
 		{
-			txBuffer[bufferIdx++] = GS_Data[it].red;
+			spiTxBuffer[bufferIdx++] = GS_Data[it].red;
 		}
 
-		txBuffer[bufferIdx++] = GS_Data[it].green;
+		spiTxBuffer[bufferIdx++] = GS_Data[it].green;
 
 		if (APA102__getRGBLedOrder() == RGB_LED_ORDER__BLUE_GREEN_RED)
 		{
-			txBuffer[bufferIdx++] = GS_Data[it].red;
+			spiTxBuffer[bufferIdx++] = GS_Data[it].red;
 		}
 		else
 		{
-			txBuffer[bufferIdx++] = GS_Data[it].blue;
+			spiTxBuffer[bufferIdx++] = GS_Data[it].blue;
 		}
 	}
 
 	/* END FRAME */
 	for (it = 0; it < STOP_FRAME_LENGTH; it++)
 	{
-		txBuffer[bufferIdx++] = 0xFF;
+		spiTxBuffer[bufferIdx++] = 0xFF;
 	}
 
-    //ret=spi_device_transmit(spi, &spiTransaction);  //Transmit!
-    ret=spi_device_queue_trans(spi, &spiTransaction, portMAX_DELAY);
-    assert(ret==ESP_OK);
-
+	if (ESP_OK != spi_device_queue_trans(spiDeviceHandle, &spiTransaction, portMAX_DELAY))
+	{
+		assert(0);
+	}
 }
 
 
@@ -187,12 +183,35 @@ void APA102__setGlobalBrightness (uint8_t brightness)
 
 void APA102__init (void)
 {
+	/* initialize SPI controller */
+
+	spiBusConfig.miso_io_num = -1;
+	spiBusConfig.mosi_io_num = GPIO_NUM_23;
+	spiBusConfig.sclk_io_num = GPIO_NUM_18;
+	spiBusConfig.quadwp_io_num = -1;
+	spiBusConfig.quadhd_io_num = -1;
+
+	spiDeviceInterfaceConfig.clock_speed_hz = 20000000;
+	spiDeviceInterfaceConfig.mode = 0;
+	spiDeviceInterfaceConfig.spics_io_num = -1;
+	spiDeviceInterfaceConfig.queue_size = 5;
+
+	if (E_OK != spi_bus_initialize(HSPI_HOST, &spiBusConfig, 1))
+	{
+		assert(0);
+	}
+
+	if (E_OK != spi_bus_add_device(HSPI_HOST, &spiDeviceInterfaceConfig, &spiDeviceHandle))
+	{
+		assert(0);
+	}
+
 #if (RGB_LED_ORDER == RGB_LED_ORDER__CONFIGURABLE)
 	RGBLedOrder = eeprom_read_byte(&RGBLedOrder_EEPROM);
 
-	if (	(RGBLedOrder != RGB_LED_ORDER__BLUE_GREEN_RED)
-		&& 	(RGBLedOrder != RGB_LED_ORDER__RED_GREEN_BLUE)
-		)
+	if (		(RGBLedOrder != RGB_LED_ORDER__BLUE_GREEN_RED)
+			&& 	(RGBLedOrder != RGB_LED_ORDER__RED_GREEN_BLUE)
+	)
 	{
 		RGBLedOrder = RGB_LED_ORDER__RED_GREEN_BLUE;
 		eeprom_update_byte(&RGBLedOrder_EEPROM, RGBLedOrder);
@@ -200,28 +219,7 @@ void APA102__init (void)
 
 #endif
 
-    esp_err_t ret;
-
-    spi_bus_config_t buscfg ={
-        .miso_io_num=-1,
-        .mosi_io_num=GPIO_NUM_23,
-        .sclk_io_num=GPIO_NUM_18,
-        .quadwp_io_num=-1,
-        .quadhd_io_num=-1
-    };
-
-    spi_device_interface_config_t devcfg={
-        .clock_speed_hz=20000000,               //Clock out at 10 MHz
-        .mode=0,                                //SPI mode 0
-        .spics_io_num=-1,               //CS pin
-        .queue_size=5,                          //We want to be able to queue 7 transactions at a time
-    };
-
-
-    spi_bus_initialize(HSPI_HOST, &buscfg, 1);
-    spi_bus_add_device(HSPI_HOST, &devcfg, &spi);
-
-    APA102__setGlobalBrightness(APA102_GLOBAL_BRIGHNESS__MAX);
+	APA102__setGlobalBrightness(APA102_GLOBAL_BRIGHNESS__MAX);
 }
 
 #endif
