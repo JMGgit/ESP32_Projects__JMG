df7991a85f9c4be8cbeaf1cfdf8cb7ce74028bab
Jean-Martin George <jeanmartin.george@gmail.com>
2018-02-27 21:29:32 +0100

    enhancement for for nvs init

--- components/Drivers/uC.c
+++ components/Drivers/uC.c
@@ -128,7 +128,18 @@ void gpio__handleInterrupt (void *para)
 void uC__init (void)
 {
 	/* init non-volatile memory */
-	nvs_flash_init();
+    esp_err_t err = nvs_flash_init();
+
+    if (err == ESP_ERR_NVS_NO_FREE_PAGES)
+    {
+        // OTA app partition table has a smaller NVS partition size than the non-OTA
+        // partition table. This size mismatch may cause NVS initialization to fail.
+        // If this happens, we erase NVS partition and initialize NVS again.
+        ESP_ERROR_CHECK(nvs_flash_erase());
+        err = nvs_flash_init();
+    }
+
+    ESP_ERROR_CHECK(err);
 
 	/* init LED for ESP32 thing board */
 	gpio_pad_select_gpio(TEST_LED_BOARD_GPIO);
